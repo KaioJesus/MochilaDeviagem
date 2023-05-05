475984d419ab19c8ec633309f182a028ce5492fa
Kaio Jesus <kaioramos990@hotmail.com>
2023-05-05 12:30:49 -0300

    localStorage para um elemento

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 
 const form = document.getElementById('novoItem');
+const lista = document.getElementById("lista");
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
     // Formuário envia os dados para algum lugar, nesse caso envia para a própria página. Preciso interromper esse comportamento, então para isso, chamao o método utilizado acima;
 
+    const nome = evento.target.elements['nome'];
+    const  quantidade = evento.target.elements['quantidade'];
 
+    criaElemento(nome.value, quantidade.value);
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+    nome.value = "";
+    quantidade.value = "";
+    // toda vez que enviar o formulario, apagar os dados escritos anteriormente
 })
 
 //validando formulário
 
 function criaElemento(nome, quantidade){
-    console.log(nome);
-    console.log(quantidade);
-
     const novoItem = document.createElement('li');
     //criar um elemento na li
      novoItem.classList.add("item");
@@ -29,5 +32,12 @@ function criaElemento(nome, quantidade){
     //para resolver o problema do objeto, insere um elemento criado dentro do outro; nesse caso o numeroItem vai entrar dentro do novo item
     novoItem.innerHTML += nome;
     //Recebe a quantidade e o nome
-    console.log(novoItem); //virou objeto porque somamos elementos criados no html pelo js, concatenando em uma variável;
+
+    lista.appendChild(novoItem);
+
+    localStorage.setItem("nome", nome);
+    localStorage.setItem("quantidade", quantidade);
+    //armazenar os dados recebidos no navegador para quando atualizar esses dados continuarem lá
+
+    // console.log(novoItem); //virou objeto porque somamos elementos criados no html pelo js, concatenando em uma variável;
 }
